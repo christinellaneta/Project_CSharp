184fc4251ca448957094007ba6f94e1bc327f1ee
christinellaneta <llaneta.christinemae@gmail.com>
2025-11-12 18:32:05 +0800

    Add Task2 class with number matrix initialization

---
+++ Project/Task2.cs
@@ -0,0 +1,15 @@
+class Task2
+{
+    static void Main(string[] args)
+    {
+        // Row 0: Even numbers
+// Row 1: Odd numbers
+int[][] numberMatrix = new int[][]
+{
+    new int[] { 2, 4, 6, 8, 10 },
+    new int[] { 1, 3, 5, 7, 9 }
+};
+
+Console.WriteLine("The number matrix has been initialized.");
+    }
+}
